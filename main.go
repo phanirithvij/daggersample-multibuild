a1428333ed8a9a52ed427c5421cb77de75403334
phanirithvij <phanirithvij2000@gmail.com>
2023-08-29 15:59:08 +0530

    init

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	if err := build(context.Background()); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func build(ctx context.Context) error {
+	// define build matrix
+	oses := []string{"linux", "darwin"}
+	arches := []string{"amd64", "arm64"}
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	// get reference to the local project
+	src := client.Host().Directory(".")
+
+	// create empty directory to put build outputs
+	outputs := client.Directory()
+
+	// get `golang` image
+	golang := client.Container().From("golang:latest")
+
+	// mount cloned repository into `golang` image
+	golang = golang.WithDirectory("/src", src).WithWorkdir("/src")
+
+	for _, goos := range oses {
+		for _, goarch := range arches {
+			// create a directory for each os and arch
+			path := fmt.Sprintf("build/%s/%s/", goos, goarch)
+
+			// set GOARCH and GOOS in the build environment
+			build := golang.WithEnvVariable("GOOS", goos)
+			build = build.WithEnvVariable("GOARCH", goarch)
+
+			// build application
+			build = build.WithExec([]string{
+				"go", "build",
+				"-buildmode=pie", "-trimpath", `-ldflags`, `-s -extldflags "-static"`,
+				"-o", path,
+			})
+
+			// get reference to build output directory in container
+			outputs = outputs.WithDirectory(path, build.Directory(path))
+		}
+	}
+	// write build artifacts to host
+	_, err = outputs.Export(ctx, ".")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
